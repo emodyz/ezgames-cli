f8564474676ce961bd967a88db32bf36d71a7c24
Saber Nouira <contact@wirk.fr>
2021-05-27 19:38:56 +0200

    check if tag is semver compliant

--- src/commands/app/upgrade.ts
+++ src/commands/app/upgrade.ts
@@ -5,6 +5,7 @@ import GitHub from '../../core/github'
 import collect from 'collect.js'
 import semver from 'semver'
 import {supportedVersions} from '../../core/env'
+import {cli} from 'cli-ux'
 
 export default class AppUpgrade extends Command {
   static description = chalk`{magenta.bold EZGames} {cyan Updater}`
@@ -18,8 +19,8 @@ export default class AppUpgrade extends Command {
   async run() {
     const gitInfo = await getGitInfo()
 
-    if (!gitInfo.isTag) {
-      throw new Error(chalk`{bgRed.white.bold \nCurrent version: "${gitInfo.current}"\nAutomatic updates are only supported when tracking GitHub releases and not a branch like "master" or "dev"}`)
+    if (!gitInfo.isTag || !semver.valid(gitInfo.current)) {
+      throw new Error(chalk`{bgRed.white.bold \nCurrent version: "${gitInfo.current}"\nAutomatic updates are only supported when tracking a "semver" compliant GitHub release and not a branch like "master" or "dev"}`)
     }
 
     const gitHubApi = process.env.EZG_CLI_GITHUB_TOKEN ? GitHub.requestWithAuth : GitHub.requestAnonymously
