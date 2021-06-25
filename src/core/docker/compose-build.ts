bdce7249e5c834df580d0cfbe56ef4cb85767d74
Saber Nouira <contact@wirk.fr>
2021-06-25 16:54:56 +0200

    use chache for rebuiling containers

--- src/core/docker/compose-build.ts
+++ src/core/docker/compose-build.ts
@@ -4,5 +4,5 @@ import {getDefaultExecaOptions} from '../defaults'
 export function dockerComposeBuild(execaOptions?: execa.Options): ExecaChildProcess {
   execaOptions = Object.assign({}, getDefaultExecaOptions(), execaOptions)
 
-  return execa('docker-compose', ['build', '--no-cache'], execaOptions)
+  return execa('docker-compose', ['build'/* , '--no-cache' */], execaOptions)
 }
