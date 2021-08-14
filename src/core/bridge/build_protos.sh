50b3ff3caadf002f2f191d5d32d10839668491a6
Saber Nouira <contact@wirk.fr>
2021-08-14 09:57:53 +0200

    let's build some protocol buffers

---
+++ src/core/bridge/build_protos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+SCRIPT_DIR=$(realpath "$0")
+BASEDIR=$(dirname "${SCRIPT_DIR}")
+#cd ${BASEDIR}/../
+echo "${BASEDIR}"
+
+PROTO_DEST=./src/core/bridge/src/proto
+
+# mkdir -p ${PROTO_DEST}
+
+# JavaScript code generation
+yarn run grpc_tools_node_protoc \
+    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+    --ts_out=grpc_js:${PROTO_DEST} \
+    --js_out=import_style=commonjs,binary:${PROTO_DEST} \
+    --grpc_out=grpc_js:${PROTO_DEST} \
+    -I "${BASEDIR}"/proto \
+    "${BASEDIR}"/proto/*.proto
