79a7b2147ae84463072cdaf5fe0ba6c95b415919
Saber Nouira <contact@wirk.fr>
2022-06-30 16:41:08 +0200

    bridge deamon

--- src/commands/bridge/start.ts
+++ src/commands/bridge/start.ts
@@ -5,23 +5,30 @@ import Pm2DaemonError = Pm2Errors.Pm2DaemonError
 import Pm2ProcessError = Pm2Errors.Pm2ProcessError
 import Pm2RpcError = Pm2Errors.Pm2RpcError
 
+// TODO: move all custom flags to "core"
+const portNum = flags.build<number>({char: 'P', default: 6660, parse: input => Number.parseInt(input, 10)})
+
 export default class BridgeStart extends Command {
   static description = 'Starts the bridge server'
 
   static flags = {
+    host: flags.string({char: 'H', default: '0.0.0.0'}),
+    port: portNum(),
     help: flags.help({char: 'h'}),
   }
 
-  // TODO: Execute "pm2 install pm2-logrotate" with cli installation
-  // TODO: Re
+  // TODO: Execute "pm2 install pm2-logrotate" with cli installation or add a cron job to avoid massive disk usage
   async run() {
+    const {flags} = this.parse(BridgeStart)
+    flags.port = flags.port ? flags.port : 6660
+
     pm2.connect(function (err) {
       if (err) {
         throw new Pm2DaemonError(err)
       }
 
       pm2.start({
-        script: 'ezgames bridge:listen --daemon',
+        script: `ezgames bridge:listen -H ${flags.host} -P ${flags.port} --daemon`,
         name: 'ezg-bridge',
       }, function (err) {
         if (err) {
